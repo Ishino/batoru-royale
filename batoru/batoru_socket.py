7a878555879fa2053de55a5ea0c9cfa507be46e6
Dries Vanlerberghe <dries.vanlerberghe@gmail.com>
2016-03-25 23:16:30 +0000

    Leave battlefield when Idle

--- batoru/batoru_socket.py
+++ batoru/batoru_socket.py
@@ -2,7 +2,7 @@ import pika
 import json
 
 from flask import session, request
-from flask_socketio import SocketIO, emit, join_room, rooms
+from flask_socketio import SocketIO, emit, join_room, rooms, leave_room
 from server.batoru_front import app
 from battlefront.battlefront import Battlefront
 
@@ -59,6 +59,19 @@ def join(message):
 
     front = Battlefront()
     front.add_player(message['player'], message['player_room'])
+    player_list = front.get_player_list()
+
+    emit('fight players', {'player': player_list}, room=message['room'], broadcast=True)
+
+@socketio.on('leave', namespace='/fight')
+def leave(message):
+    leave_room(message['room'])
+    emit('fight status',
+         {'data': message['player'] + ' left the battlefield.', 'player': message['player']},
+         room=message['room'])
+
+    front = Battlefront()
+    front.remove_player(message['player'])
     player_list = front.get_player_list()
 
     emit('fight players', {'player': player_list}, room=message['room'], broadcast=True)
