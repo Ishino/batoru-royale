622ca2955691b3eea256654ab18fea992b553afd
Dries Vanlerberghe <dries.vanlerberghe@gmail.com>
2016-04-02 14:34:46 +0100

    Player view

--- batoru/server/batoru_front.py
+++ batoru/server/batoru_front.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, send_from_directory
+from battlefront.player import Player
 
 app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = 'secret!'
@@ -13,6 +14,11 @@ def index():
 def canvas():
     return render_template('canvas.html')
 
+@app.route('/player/<name>')
+def player(name):
+    player = Player()
+    player.load_player(name)
+    return render_template('player.html', player=player)
 
 @app.route('/<path:filename>')
 def send_static(filename):
