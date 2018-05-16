c08ce697a1ed8dc9b2ff1263c634c23f723070a1
Dries Vanlerberghe <dries.vanlerberghe@capgemini.com>
2018-05-16 21:08:04 +0100

    Codestyle fixes W503 vs W504

--- batoru/simulator/battle.py
+++ batoru/simulator/battle.py
@@ -163,8 +163,9 @@ class Battle:
                 player_one.set_experience_calculator(TournamentExperience())
                 player_two = self.create_player(match[1])
                 player_two.set_experience_calculator(TournamentExperience())
-                winner = self.compete(str(tournament.tournament_id) + "."
-                                      + str(tournament_fight_id), player_one, player_two)
+                compete_id = str(tournament.tournament_id) + "."\
+                    + str(tournament_fight_id)
+                winner = self.compete(compete_id, player_one, player_two)
                 tournament.register_win(winner.name)
 
                 self.fight.logLevel = 1
@@ -179,10 +180,11 @@ class Battle:
 
         for player in players:
             fighter = self.player_engine.load_player(player)
-            self.fight.print_event(" [ " + str(fighter.name) + " ] level: " + str(fighter.level) + " exp: "
-                                   + str(fighter.experience) + "/"
-                                   + str(TournamentExperience().calculate_experience_need(fighter.level))
-                                   + " type: " + str(fighter.type) + "\n", 0)
+            event_string = " [ " + str(fighter.name) + " ] level: " + str(fighter.level) + " exp: " \
+                           + str(fighter.experience) + "/" \
+                           + str(TournamentExperience().calculate_experience_need(fighter.level)) \
+                           + " type: " + str(fighter.type) + "\n"
+            self.fight.print_event(event_string, 0)
 
     def compete(self, fight_id, player_one: Fighter, player_two: Fighter, scroll=False):
 
